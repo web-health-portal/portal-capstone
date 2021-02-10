3137684c22602a25e800cf5134edecc6e2bacf02
Kyle Mayer <kmayer1@cnm.edu>
2021-02-10 11:28:01 -0700

    add NavigationBar component to Home.js

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,8 +1,10 @@
 import React from "react"
+import {NavigationBar} from "./NavigationBar";
 
 export const Home = () => {
     return (
         <>
+            <NavigationBar/>
             <h1>Home</h1>
         </>
     )
