8f5ea0203e4bf43f065b82ffb483d7de07145561
Kyle Mayer <kmayer1@cnm.edu>
2021-02-10 11:27:32 -0700

    extracted LanguageToggle to its own functional React component

---
+++ frontend/src/ui/LanguageToggle.js
@@ -0,0 +1,37 @@
+import {ButtonGroup, Container, Row, ToggleButton} from "react-bootstrap";
+import React, {useState} from "react";
+
+
+export const LanguageToggle = () => {
+    const [radioValue, setRadioValue] = useState('1');
+
+//for English buttons
+    const radioButtons = [
+        {name: 'English', value: '1'},
+        {name: 'Español', value: '2'},
+    ];
+
+    return (
+        <>
+            <Container fluid>
+                <Row className={"justify-content-center py-2 bg-dark"}>
+                    <ButtonGroup toggle>
+                        {radioButtons.map((radio, idx) => (
+                            <ToggleButton
+                                key={idx}
+                                type="radio"
+                                variant="primary"
+                                name="radio"
+                                value={radio.value}
+                                checked={radioValue === radio.value}
+                                onChange={(e) => setRadioValue(e.currentTarget.value)}
+                            >
+                                {radio.name}
+                            </ToggleButton>
+                        ))}
+                    </ButtonGroup>
+                </Row>
+            </Container>
+        </>
+    )
+}
