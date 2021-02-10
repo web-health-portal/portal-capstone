9ca818ff650edf3d6d140ea519b6906847ae5a67
Kyle Mayer <kmayer1@cnm.edu>
2021-02-10 16:37:14 -0700

    adding routing for Account

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
+import {Account} from "./Account";
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
+                <Route exact path='/account' component={Account}/>
                 <Route exact path='/' component={Home} />
                 <Route component={FourOhFour} />
             </Switch>
