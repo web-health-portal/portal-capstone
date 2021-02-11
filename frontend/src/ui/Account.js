289068e910c237593787dd6b1bd1ec40184b70b2
Kyle Mayer <kmayer1@cnm.edu>
2021-02-11 10:04:08 -0700

    first rough draft of Account static page

--- frontend/src/ui/Account.js
+++ frontend/src/ui/Account.js
@@ -4,6 +4,7 @@ import {Form, Button, Col, Container, FormControl, FormGroup, Row} from "react-b
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faUser, faHome, faSignOutAlt, faCamera} from "@fortawesome/free-solid-svg-icons";
+import {Article} from "./shared/components/Article";
 
 library.add(faUser, faHome, faSignOutAlt, faCamera);
 
@@ -12,6 +13,27 @@ const navLinks = [
     {name: "Log Out", linkTo: "/log-out"},
 ];
 
+// temporary placeholder to show an example article
+const placeHolderArticle = {
+    //Topic id to search by: 30615
+    //Link to English version: https://health.gov/myhealthfinder/api/v3/topicsearch.json?lang=en&topicId=30615
+    //Link to Spanish version: https://health.gov/myhealthfinder/api/v3/topicsearch.json?lang=es&topicId=30615
+    articleImageUrl: "https://health.gov/sites/default/files/2020-01/losingweight-2.jpg",
+    articleEnglishImageAlt: "Health care provider talking with woman",
+    articleEnglishTitle: "Losing Weight: Questions for the doctor",
+    articleEnglishDate: "1580741304",
+    articleSpanishTitle: "Preguntas para el doctor: Bajar de peso",
+    articleSpanishDate: "1580741304",
+    articleEnglishUrl: "https://health.gov/myhealthfinder/topics/health-conditions/obesity/losing-weight-questions-doctor",
+    articleSpanishImageAlt: "Preguntas para el doctor: Bajar de peso",
+    articleSpanishUrl: "https://health.gov/espanol/myhealthfinder/temas/enfermedades-problemas-salud/obesidad/preguntas-doctor-bajar-peso",
+    articleCategories: [
+        {
+            categoryEnglishName: "Doctor Visits, Obesity, Talking with the Doctor, Nutrition, Physical Activity",
+            categorySpanishName: "Consultas con el doctor, Obesidad, Charlas con el doctor, Nutrición, Actividad física"
+        },
+    ],
+}
 
 export const Account = () => {
     return (
@@ -20,10 +42,12 @@ export const Account = () => {
             <h1 className={"mx-3"}>Account</h1>
             <Container as={"section"} fluid className={"p-3"}>
                 <Row>
-                    <Col md={4} className={"border-right border-dark"}>
-                        <FontAwesomeIcon icon={faUser} size="10x" className={"p-4 border rounded-circle"}/>
-                        <FontAwesomeIcon icon={faCamera} size="lg"/>
-                        <Row className={"mt-1"}>
+                    <Col md={4} className={"border-right border-dark md-auto"}>
+                        <Row className={"justify-content-center"}>
+                            <FontAwesomeIcon icon={faUser} size="10x" className={"p-4 border d-block rounded-circle"}/>
+                            <FontAwesomeIcon icon={faCamera} size="lg d-block"/>
+                        </Row>
+                        <Row className={"mt-1 justify-content-center"}>
                             <Form className={"px-3"}>
                                 <FormGroup>
                                     <Form.Label>Name:</Form.Label>
@@ -46,17 +70,34 @@ export const Account = () => {
                         <Row className={"mt-1"}>
                         </Row>
                     </Col>
-                    <Col md={8}>
+                    <Col md={8} className={"pt-3"}>
                         <h3>Search Saved Articles</h3>
                         <Row>
                             <Form inline className={"mx-3"}>
                                 <FormGroup>
-                                    <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                                    <FormControl type="text" placeholder="Search" className="mr-sm-2 py-3"/>
                                     <Button variant="primary">Search</Button>
                                 </FormGroup>
                             </Form>
                         </Row>
-
+                        <Row>
+                            <Container>
+                                <Row>
+                                    <Col lg={6}>
+                                        <Article articles={placeHolderArticle}/>
+                                        <Article articles={placeHolderArticle}/>
+                                        <Article articles={placeHolderArticle}/>
+                                        <Article articles={placeHolderArticle}/>
+                                    </Col>
+                                    <Col lg={6}>
+                                        <Article articles={placeHolderArticle}/>
+                                        <Article articles={placeHolderArticle}/>
+                                        <Article articles={placeHolderArticle}/>
+                                        <Article articles={placeHolderArticle}/>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Row>
                     </Col>
                 </Row>
 
