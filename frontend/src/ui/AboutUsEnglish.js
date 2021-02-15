64ddf915dc520748475e287c11bd1655f7045c63
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-02-14 22:56:41 -0700

    added icons and fix some padding

--- frontend/src/ui/AboutUsEnglish.js
+++ frontend/src/ui/AboutUsEnglish.js
@@ -2,6 +2,8 @@ import React from "react";
 import {NavigationBar} from "./shared/components/NavigationBar";
 import {Container, Image, Row, Col} from "react-bootstrap";
 import profileicon from "./profileicon.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {fal, faLink} from '@fortawesome/free-solid-svg-icons'
 
 const navLinks = [
     {name: "Home", linkTo: "/Home"},
@@ -17,6 +19,7 @@ export const AboutUs = () => {
                 <Container className="text-center mt-4 mb-5">
                     <h1>Developers</h1>
                     <p> Tiramisu biscuit cake gingerbread bear claw macaroon gummi bears. Liquorice chocolate I love marzipan. Pudding macaroon chocolate liquorice cake cotton candy caramels chocolate bar marshmallow. Marzipan gingerbread candy danish muffin dessert. I love cookie I love I love. Chocolate bar dragée cotton candy. Ice cream muffin caramels liquorice cupcake biscuit gummi bears. Chocolate marzipan lemon drops soufflé I love.</p>
+
                 </Container>
 
             {/*DEVELOPERS NAME & DETAILS*/}
@@ -24,37 +27,43 @@ export const AboutUs = () => {
             <Container>
                 <Row className="justify-content-center align-content-center">
 
-                    {/*Col #1 IMAGE*/}
+                    {/*Developer #1 IMAGE*/}
                         <Col xs={6} md={4} lg={3}>
                             <Image className="img-fluid" src={profileicon} roundedCircle alt="Profile Picture"/>
                         </Col>
 
-                    {/*Col #1 TEXT*/}
-                        <Col className="mt-4" xs={7} md={6} lg={8}>
+                    {/*Developer #1 TEXT*/}
+                        <Col className="mt-4" xs={9} md={6} lg={9}>
                             <h2>Name</h2>
                             <p>Tiramisu biscuit cake gingerbread bear claw macaroon gummi bears. Liquorice chocolate I love marzipan. Pudding macaroon chocolate liquorice cake cotton candy caramels chocolate bar marshmallow. Marzipan gingerbread candy danish muffin dessert. I love cookie I love I love. Chocolate bar dragée cotton candy. Ice cream muffin caramels liquorice cupcake biscuit gummi bears.</p>
+                            <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <FontAwesomeIcon icon={faLink} size="3x"/>
                         </Col>
 
-                    {/*Col #2 IMAGE*/}
+                    {/*Developer #2 IMAGE*/}
                         <Col xs={6} md={4} lg={3}>
                             <Image className="img-fluid" src={profileicon} roundedCircle alt="Profile Picture"/>
                         </Col>
 
-                    {/*Col #2 TEXT*/}
-                        <Col className="mt-4" xs={7} md={6} lg={8}>
+                    {/*Developer #2 TEXT*/}
+                        <Col className="mt-4" xs={9} md={6} lg={9}>
                             <h2>Name</h2>
                             <p>Tiramisu biscuit cake gingerbread bear claw macaroon gummi bears. Liquorice chocolate I love marzipan. Pudding macaroon chocolate liquorice cake cotton candy caramels chocolate bar marshmallow. Marzipan gingerbread candy danish muffin dessert. I love cookie I love I love. Chocolate bar dragée cotton candy. Ice cream muffin caramels liquorice cupcake biscuit gummi bears.</p>
+                            <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <FontAwesomeIcon icon={faLink} size="3x"/>
                         </Col>
 
-                    {/*Col #3 IMAGE*/}
+                    {/*Developer #3 IMAGE*/}
                         <Col className="mt-4" xs={6} md={4} lg={3}>
                             <Image className="img-fluid" src={profileicon} roundedCircle alt="Profile Picture"/>
                         </Col>
 
-                    {/*Col #3 TEXT*/}
-                        <Col xs={7} md={10} lg={8}>
+                    {/*Developer #3 TEXT*/}
+                        <Col xs={9} md={10} lg={9}>
                             <h2>Name</h2>
                             <p>Tiramisu biscuit cake gingerbread bear claw macaroon gummi bears. Liquorice chocolate I love marzipan. Pudding macaroon chocolate liquorice cake cotton candy caramels chocolate bar marshmallow. Marzipan gingerbread candy danish muffin dessert. I love cookie I love I love. Chocolate bar dragée cotton candy. Ice cream muffin caramels liquorice cupcake biscuit gummi bears.</p>
+                            <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <FontAwesomeIcon icon={faLink} size="3x"/>
                         </Col>
                 </Row>
             </Container>
