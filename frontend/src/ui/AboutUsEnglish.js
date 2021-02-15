30b529bc8c4df8c0714314fdd0c692ddecc13032
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-02-15 11:41:23 -0700

    added and installed  brand svg icons and made changes to about us english page

--- frontend/src/ui/AboutUsEnglish.js
+++ frontend/src/ui/AboutUsEnglish.js
@@ -2,8 +2,10 @@ import React from "react";
 import {NavigationBar} from "./shared/components/NavigationBar";
 import {Container, Image, Row, Col} from "react-bootstrap";
 import profileicon from "./profileicon.png"
+import kylemayerprofilepicture from "./kyle-mayer-profile-picture.jpg"
+import ramonprofilepicture from "./ramon-profile-picture.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faLink, fa} from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faLinkedinIn} from "@fortawesome/free-brands-svg-icons";
 import './style.css'
 
 
@@ -33,59 +35,59 @@ export const AboutUs = () => {
 
                     {/*TEXT #1*/}
                         <Col className="mb-4" xs={9} md={6} lg={9}>
-                            <h2 className="mt-4">Name</h2>
+                            <h2 className="mt-4">Elizabeth Hines</h2>
                             <p>Tiramisu biscuit cake gingerbread bear claw macaroon gummi bears. Liquorice chocolate I love marzipan. Pudding macaroon chocolate liquorice cake cotton candy caramels chocolate bar marshmallow. Marzipan gingerbread candy danish muffin dessert. I love cookie I love I love. Chocolate bar dragée cotton candy. Ice cream muffin caramels liquorice cupcake biscuit gummi bears.</p>
 
                             {/*Link to LinkedIn*/}
-                            <a href="https://www.linkedin.com/" className="linkedIn">
-                            <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <a href="https://www.linkedin.com/in/elizabethhines2140/" className="linkedIn">
+                            <FontAwesomeIcon icon={faLinkedinIn} size="3x"/>
                             </a>
 
                             {/*Link to GitHub*/}
-                            <a href="https://github.com/" className="GitHub">
-                                <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <a href="https://github.com/LizzyHines" className="GitHub">
+                                <FontAwesomeIcon icon={faGithub} size="3x"/>
                             </a>
                         </Col>
 
                     {/*Developer #2*/}
                         <Col className="mb-3" xs={6} md={4} lg={3}>
-                            <Image className="img-fluid" src={profileicon} roundedCircle alt="Profile Picture"/>
+                            <Image className="img-fluid" src={kylemayerprofilepicture} roundedCircle alt="Profile Picture"/>
                         </Col>
 
                     {/*TEXT #2*/}
                         <Col className="mb-4" xs={9} md={6} lg={9}>
-                            <h2 className="mt-4">Name</h2>
-                            <p>Tiramisu biscuit cake gingerbread bear claw macaroon gummi bears. Liquorice chocolate I love marzipan. Pudding macaroon chocolate liquorice cake cotton candy caramels chocolate bar marshmallow. Marzipan gingerbread candy danish muffin dessert. I love cookie I love I love. Chocolate bar dragée cotton candy. Ice cream muffin caramels liquorice cupcake biscuit gummi bears.</p>
+                            <h2 className="mt-4">Kyle Mayer</h2>
+                            <p>Passionate and creative problem solver working to expand my skill set as much as possible every single day. My previous career as a professional chef taught me how to be organized, methodical, and a team player in order to deliver quality products in a timely manner.</p>
 
                             {/*Link to LinkedIn*/}
-                            <a href="https://www.linkedin.com/" className="linkedIn">
-                                <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <a href="https://www.linkedin.com/in/kyle-mayer-fullstack/" className="linkedIn">
+                                <FontAwesomeIcon icon={faLinkedinIn} size="3x"/>
                             </a>
 
                             {/*Link to GitHub*/}
-                            <a href="https://github.com/" className="GitHub">
-                                <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <a href="https://github.com/kmayer5120" className="GitHub">
+                                <FontAwesomeIcon icon={faGithub} size="3x"/>
                             </a>
                         </Col>
 
                     {/*Developer #3*/}
                         <Col className="mb-3" xs={6} md={4} lg={3}>
-                            <Image className="img-fluid" src={profileicon} roundedCircle alt="Profile Picture"/>
+                            <Image className="img-fluid" src={ramonprofilepicture} roundedCircle alt="Profile Picture"/>
                         </Col>
 
                     {/*TEXT #3*/}
                         <Col xs={9} md={10} lg={9}>
-                            <h2 className="mt-4">Name</h2>
-                            <p>Tiramisu biscuit cake gingerbread bear claw macaroon gummi bears. Liquorice chocolate I love marzipan. Pudding macaroon chocolate liquorice cake cotton candy caramels chocolate bar marshmallow. Marzipan gingerbread candy danish muffin dessert. I love cookie I love I love. Chocolate bar dragée cotton candy. Ice cream muffin caramels liquorice cupcake biscuit gummi bears.</p>
+                            <h2 className="mt-4">Ramon Pantoja</h2>
+                            <p>I am seeking a Junior Web Developer position where I can enhance my skillset in different web technologies. I am a motivated self-starter with an aptitude for continuous learning and an ability to multi-task, ensuring projects completed efficiently and in a timely manner. Great team player with success in several fast-paced and challenging environments</p>
 
                             {/*Link to LinkedIn*/}
-                            <a href="https://www.linkedin.com/" className="linkedIn">
-                                <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <a href="https://www.linkedin.com/in/ramon-pantoja/" className="linkedIn">
+                                <FontAwesomeIcon icon={faLinkedinIn} size="3x"/>
                             </a>
 
                             {/*Link to GitHub*/}
-                            <a href="https://github.com/" className="GitHub">
-                                <FontAwesomeIcon icon={faLink} size="3x"/>
+                            <a href="https://github.com/RamonPantoja12" className="GitHub">
+                                <FontAwesomeIcon icon={faGithub} size="3x"/>
                             </a>
                         </Col>
                 </Row>
