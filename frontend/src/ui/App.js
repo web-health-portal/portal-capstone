b0f2d62dbcbfa36ddd424aaa2c27c21841ec4cf0
Kyle Mayer <kmayer1@cnm.edu>
2021-02-16 14:50:12 -0700

    added navigation bar component to App.js

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,17 +1,19 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Route, Switch } from 'react-router'
-import { Home } from './Home'
-import { FourOhFour } from './FourOhFour'
+import {BrowserRouter} from 'react-router-dom'
+import {Route, Switch} from 'react-router'
+import {Home} from './Home'
+import {FourOhFour} from './FourOhFour'
 import React from 'react'
 import {Account} from "./Account";
+import {NavigationBar} from "./shared/components/NavigationBar";
 
 export const App = () => (
     <>
         <BrowserRouter>
+            <NavigationBar/>
             <Switch>
                 <Route exact path='/account' component={Account}/>
-                <Route exact path='/' component={Home} />
-                <Route component={FourOhFour} />
+                <Route exact path='/' component={Home}/>
+                <Route component={FourOhFour}/>
             </Switch>
         </BrowserRouter>
 
