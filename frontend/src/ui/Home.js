154975d0595df88993598d403ab5b9e3ebc568d8
Elizabeth Hines <ehines5@cnm.edu>
2021-02-15 21:50:41 -0700

    transferred to Home file

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,5 +1,7 @@
 import React from "react"
 import {NavigationBar} from "./shared/components/NavigationBar";
+import {Button, Container, Image, Row, Col, Form} from "react-bootstrap";
+
 
 const navLinks = [
     {name: "Sign Up", linkTo: "/sign-up"},
@@ -10,7 +12,6 @@ export const Home = () => {
     return (
         <>
             <NavigationBar links={navLinks}/>
-            <h1>Home</h1>
-        </>
+            <h1 className={mx-3}>Welcome</h1>
     )
 }
