49b46962d1d17e3223725902a2e7a7350683e5dc
Elizabeth Hines <ehines5@cnm.edu>
2021-02-16 13:59:20 -0700

    made changes to Home.js

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,7 +1,8 @@
 import React from "react"
 import {NavigationBar} from "./shared/components/NavigationBar";
 import {Button, Container, Image, Row, Col, Form, FormLabel} from "react-bootstrap";
-import {Article} from "./shared/components/Article";
+// import {Article} from "./shared/components/Article";
+import backgroundimage from "./background image.png";
 
 const navLinks = [
     {name: "Sign Up", linkTo: "/sign-up"},
@@ -13,30 +14,31 @@ export const Home = () => {
         <>
             <NavigationBar links={navLinks}/>
             <Container>
-                <FormGroup className={}>
-                    <FormLabel>Search for articles on a variety of health topics.</FormLabel>
-                    <FormControl type="text" placeholder="Search Health Topics and Articles"/>
-                    <Button variant="primary" size="sm"}>Search</Button>
+
+                <Form.Group>
+                    <Form.Label>Search for articles on a variety of health topics.</Form.Label>
+                    <Form.Control type="text" placeholder="Search Health Topics and Articles"/>
+                    <Button variant="primary" size="sm">Search</Button>
                 <p>SaluDuo is a bilingual health portal with articles in both English and Spanish, covering a variety of topics to keep you informed and healthy. Search here to find articles from our API, and make an account with us to build your own library of articles you'd like to come back to.</p>
-                </FormGroup>
+                </Form.Group>
             </Container>
 
             <Container>
                 <h4>Popular Articles</h4>
-                <Row>
-                    <Col md={6}>
-                        <Article articles={placeHolderArticle}/>
-                        <Article articles={placeHolderArticle}/>
-                        <Article articles={placeHolderArticle}/>
-                        <Article articles={placeHolderArticle}/>
-                    </Col>
-                    <Col md={6}>
-                        <Article articles={placeHolderArticle}/>
-                        <Article articles={placeHolderArticle}/>
-                        <Article articles={placeHolderArticle}/>
-                        <Article articles={placeHolderArticle}/>
-                    </Col>
-                </Row>
+                {/*<Row>*/}
+                {/*    <Col md="6">*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*    </Col>*/}
+                {/*    <Col md="6">*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*        <Article articles={placeHolderArticle}/>*/}
+                {/*    </Col>*/}
+                {/*</Row>*/}
             </Container>
         </>
     )
