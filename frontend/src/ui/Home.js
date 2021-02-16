464799ded96fc62c7d55dc15e92def83a4eab953
Elizabeth Hines <ehines5@cnm.edu>
2021-02-15 23:40:32 -0700

    heading and search bar

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {NavigationBar} from "./shared/components/NavigationBar";
 import {Button, Container, Image, Row, Col, Form} from "react-bootstrap";
-
+import {Article} from "./shared/components/Article";
 
 const navLinks = [
     {name: "Sign Up", linkTo: "/sign-up"},
@@ -12,6 +12,25 @@ export const Home = () => {
     return (
         <>
             <NavigationBar links={navLinks}/>
-            <h1 className={mx-3}>Welcome</h1>
+            <Container
+            <h2 className={"justify-content-center"}>Search Health Topics and Articles</h2>
+
+// Popular
+            <Container>
+                <Row>
+                    <Col lg={6}>
+                        <Article={placeHolderArticle}/>
+                        <Article={placeHolderArticle}/>
+                        <Article={placeHolderArticle}/>
+                        <Article={placeHolderArticle}/>
+                    </Col>
+                    <Col lg={6}>
+                        <Article={placeHolderArticle}/>
+                        <Article={placeHolderArticle}/>
+                        <Article={placeHolderArticle}/>
+                        <Article={placeHolderArticle}/>
+                    </Col>
+                </Row>
+            </Container>
     )
 }
