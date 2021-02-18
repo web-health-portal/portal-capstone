551921f82915fbdfa639b45cff0c43171c812dd2
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-02-18 10:20:31 -0700

    modified footer js and component.css

--- frontend/src/ui/shared/components/Footer.js
+++ frontend/src/ui/shared/components/Footer.js
@@ -6,17 +6,29 @@ import "./component.css"
 export const Footer = () => {
     return (
         <>
+        <section>
+            <div id="footer">
+                <ModalFooter  className="modal-footer ">
+                    <Col xs={9}  lg={6}>
+                        <p>All resources were provided by</p>
+                    </Col>
 
-            <ModalFooter  className="Footer mt-5 justify-content-center align-content-center">
+                    <Col xs={9}  lg={12}>
+                        <a className="logo mt-2" href="https://health.gov/myhealthfinder">
+                            <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/>
+                        </a>
+                    </Col>
 
-                <a className="logo" href="https://health.gov/myhealthfinder">
-                    <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/>
-                </a>
-                <a href="http://localhost:3000/about-us">
-                    <p className="devs">Meet the Developers</p>
-                </a>
+                    <Col xs={9}  lg={2}>
+                        <a href="http://localhost:3000/about-us">
+                            <p className="devs">Meet the Developers</p>
+                        </a>
+                    </Col>
+                </ModalFooter>
+            </div>
+        </section>
 
-            </ModalFooter>
+
 
             {/*<ModalFooter  className="Footer mt-5 justify-content-center align-content-center">*/}
             {/*    <Col xs={6}  lg={5}>*/}
