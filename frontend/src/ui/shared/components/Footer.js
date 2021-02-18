fe828fce00dc578b1720bf48288ec5149530a6dc
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-02-18 13:47:42 -0700

    fixed typo and cleaned file

--- frontend/src/ui/shared/components/Footer.js
+++ frontend/src/ui/shared/components/Footer.js
@@ -21,27 +21,13 @@ export const Footer = () => {
 
                         <ul>
                             <li className="mb-3">
-                                Site structured designed by <a className="logo ml-2" href="http://localhost:3000/about-us">SaluDuo</a>
+                                Site structure designed by <a className="logo ml-2" href="http://localhost:3000/about-us">SaluDuo</a>
                             </li>
                         </ul>
                     </div>
                 </ModalFooter>
             </div>
         </section>
-
-
-            {/*<ModalFooter  className="Footer mt-5 justify-content-center align-content-center">*/}
-            {/*    <Col xs={6}  lg={5}>*/}
-            {/*    <a className="logo" href="https://health.gov/myhealthfinder">*/}
-            {/*        <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/>*/}
-            {/*    </a>*/}
-            {/*    </Col>*/}
-            {/*<Col xs={8}  lg={6} className="mr-2">*/}
-            {/*    <a href="http://localhost:3000/about-us">*/}
-            {/*        <p className="devs">Meet the Developers</p>*/}
-            {/*    </a>*/}
-            {/*</Col>*/}
-            {/*</ModalFooter>*/}
 
         </>
     )
