e12f086f27ee5a0988265fce1b5c37ffd2672217
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-02-18 17:25:14 -0700

    fixed responsive footer

--- frontend/src/ui/shared/components/Footer.js
+++ frontend/src/ui/shared/components/Footer.js
@@ -1,33 +1,28 @@
 import React from "react"
-import {ModalFooter} from "react-bootstrap";
+import {Col, ModalFooter, Row} from "react-bootstrap";
 import "./component.css"
 
 
 export const Footer = () => {
     return (
         <>
-        <section>
-            <div id="footer">
-                <ModalFooter  className="modal-footer justify-content-center ">
-                    <div>
-                        <ul className="list-group">
-                            <li className="mb-3 mt-3">
-                                All health information and articles provided by
-                                <a className="logo ml-2" href="https://health.gov/myhealthfinder">
-                                    <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/>
-                                </a>
-                            </li>
-                        </ul>
+            <section>
+                <div className="modal-footer justify-content-center align-content-center my-3 border-0">
+                    <Row className="justify-content-center align-content-center ml-5">
 
-                        <ul>
-                            <li className="mb-3">
-                                Site structure designed by <a className="logo ml-2" href="http://localhost:3000/about-us">SaluDuo</a>
-                            </li>
-                        </ul>
-                    </div>
-                </ModalFooter>
-            </div>
-        </section>
+                        <Col xs={12}  lg={8}>
+                           <p className="PF">All health information and articles provided by<a className="logo ml-2" href="https://health.gov/myhealthfinder">
+                               <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/></a>
+                           </p>
+                        </Col>
+
+                        <Col xs={12}  lg={5}>
+                            <p className="PF">Site structure designed by<a className="logo ml-2" href="http://localhost:3000/about-us">SaluDuo</a></p>
+                        </Col>
+
+                    </Row>
+                </div>
+            </section>
 
         </>
     )
