2aabccee74c4711d39a68ca8742755b6e215c88c
Kyle Mayer <kmayer1@cnm.edu>
2021-02-19 08:22:30 -0700

    fixed routing real quick

--- frontend/src/ui/shared/components/NavigationBar.js
+++ frontend/src/ui/shared/components/NavigationBar.js
@@ -11,7 +11,7 @@ export const NavigationBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Link to="/home" className={"nav-link px-3"}>Home</Link>
+                        <Link to="/" className={"nav-link px-3"}>Home</Link>
                         <Link to="/account" className={"nav-link px-3"}>Account</Link>
                         <Link to="/log-in" className={"nav-link px-3"}>Log In</Link>
                         <Link to="/log-out" className={"nav-link px-3"}>Log Out</Link>
