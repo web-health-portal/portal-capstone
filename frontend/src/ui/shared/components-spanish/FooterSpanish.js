047d01da008f5f584272418d8560258a39ecf1ec
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-02-19 22:12:47 -0700

    modified responsivness on spanish footer

--- frontend/src/ui/shared/components-spanish/FooterSpanish.js
+++ frontend/src/ui/shared/components-spanish/FooterSpanish.js
@@ -7,17 +7,16 @@ export const FooterSpanish = () => {
     return (
         <>
             <section>
-                <div className="modal-footer justify-content-center align-content-center my-3 border-0">
-                    <Row className="justify-content-center align-content-center ml-5">
+                <div className="modal-footer  my-3 border-0 justify-content-lg-center flex-column">
+                    <Row className="justify-content-center align-content-center">
+                        <Col xs={10} md={9} lg={5}>
+                            <p className="PF">La estructura del sitio fue diseñada por<a className="logo ml-2" href="http://localhost:3000/about-us">SaluDuo</a></p>
+                        </Col>
 
-                        <Col xs={11} lg={9} md={9}>
+                        <Col xs={10} md={9} lg={8} >
                             <p className="PF">Toda la información y los artículos de salud fueron proporcionados por<a className="logo ml-2" href="https://health.gov/myhealthfinder">
                                 <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/></a>
                             </p>
-                        </Col>
-
-                        <Col xs={11}  lg={5} md={9}>
-                            <p className="PF">La estructura del sitio fue diseñada por<a className="logo ml-2" href="http://localhost:3000/about-us">SaluDuo</a></p>
                         </Col>
 
                     </Row>
