44bbc9e0c7a36845b78443692bf81d8bbd6ae956
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-02-19 22:19:20 -0700

    modified responsivness of footer

--- frontend/src/ui/shared/components/Footer.js
+++ frontend/src/ui/shared/components/Footer.js
@@ -8,16 +8,16 @@ export const Footer = () => {
         <>
             <section>
                 <div className="modal-footer justify-content-center align-content-center my-3 border-0">
-                    <Row className="justify-content-center align-content-center ml-5">
-
-                        <Col xs={12} lg={8}>
-                           <p className="PF">All health information and articles provided by<a className="logo ml-2" href="https://health.gov/myhealthfinder">
-                               <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/></a>
-                           </p>
-                        </Col>
+                    <Row className="justify-content-center align-content-center">
 
                         <Col xs={12}  lg={5} md={7}>
                             <p className="PF">Site structure designed by<a className="logo ml-2" href="http://localhost:3000/about-us">SaluDuo</a></p>
+                        </Col>
+
+                        <Col xs={12} lg={10}>
+                           <p className="PF">All health information and articles were provided by<a className="logo ml-2" href="https://health.gov/myhealthfinder">
+                               <img width="200px" src="https://health.gov/themes/custom/healthfinder/images/MyHF.svg" alt="Health Finder Logo"/></a>
+                           </p>
                         </Col>
 
                     </Row>
