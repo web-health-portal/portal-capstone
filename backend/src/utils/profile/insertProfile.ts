5751e05447bc186893567599f4d254b12e613a33
Kyle Mayer <kmayer1@cnm.edu>
2021-02-23 09:54:58 -0700

    added template for insertProfile

---
+++ backend/src/utils/profile/insertProfile.ts
@@ -0,0 +1,18 @@
+import {connect} from "../database.utils";
+import {Profile} from "../interfaces/Profile";
+
+
+export async function insertProfile(profile: Profile) : Promise<string>{
+    try {
+        //establish connection with the database
+        const mySqlConnection = await connect();
+        //query is a string for holding a sql statement (prepared statement)
+        const query : string = "define insert sql statement here";
+        const [rows] = await mySqlConnection.execute(query, profile);
+        return "profile successfully inserted";
+    }
+    catch(error) {
+        console.error(error.msg);
+        return error.msg;
+    }
+}
