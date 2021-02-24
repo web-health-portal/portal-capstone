870dadcd3f8ff8924003a10cda9f905e2f8bad8b
Kyle Mayer <kmayer1@cnm.edu>
2021-02-24 14:39:45 -0700

    fixed copy paste error in insertArticle

--- backend/src/utils/article/insertArticle.ts
+++ backend/src/utils/article/insertArticle.ts
@@ -1,15 +1,14 @@
 import {connect} from "../database.utils";
 import {Article} from "../interfaces/Article";
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
-
-export async function insertProfile(article: Article) : Promise<string>{
+export async function insertArticle(article: Article) : Promise<string>{
     try {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
         const query : string = `INSERT into article (articleId, articleEnglishTitle, articleEnglishDate, articleEnglishImageUrl, articleEnglishImageAlt, articleSpanishTitle, articleSpanishDate, articleSpanishImageUrl, articleSpanishUrl)
-            VALUES((UUID_TO_BIN(${uuid.v4()}), :articleEnglishId, :articleEnglishTitle, :articleEnglishDate, :articleEnglishImageUrl, :articleEnglishImageAlt, :articleSpanishTitle, :articleSpanishDate, :articleSpanishUrl)`;
+            VALUES((UUID_TO_BIN(${uuidv4()}), :articleEnglishId, :articleEnglishTitle, :articleEnglishDate, :articleEnglishImageUrl, :articleEnglishImageAlt, :articleSpanishTitle, :articleSpanishDate, :articleSpanishUrl)`;
         const [rows] = await mySqlConnection.execute(query, article);
         return "profile successfully inserted";
     }
