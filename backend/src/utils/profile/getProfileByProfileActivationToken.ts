960d34d5926addeca7e09e21dcd41f2db451e8e0
Kyle Mayer <kmayer1@cnm.edu>
2021-02-25 16:33:55 -0700

    updated get profile MySQL enabled functions

---
+++ backend/src/utils/profile/getProfileByProfileActivationToken.ts
@@ -0,0 +1,17 @@
+import {connect} from "../database.utils";
+
+export async function getProfileByProfileActivationToken(profileActivationToken: string) {
+	try {
+		const mysqlConnection = await connect();
+
+		const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken,' +
+			' profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileImage ' +
+			'FROM profile WHERE profileActivationToken = :profileActivationToken', {profileActivationToken});
+
+		// @ts-ignore is required so that rows can be interacted with like the array it is
+		return rows.length !== 0 ? {...rows[0]} : undefined;
+	} catch (e) {
+		console.error(e)
+		return undefined
+	}
+}
