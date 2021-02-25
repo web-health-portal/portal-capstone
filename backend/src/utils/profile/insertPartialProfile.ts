00e253f42e50a4da266b2cc089d3d92c976daae0
Kyle Mayer <kmayer1@cnm.edu>
2021-02-25 14:09:44 -0700

    changes to insert functions for profile and partial profile

--- backend/src/utils/profile/insertPartialProfile.ts
+++ backend/src/utils/profile/insertPartialProfile.ts
@@ -3,17 +3,16 @@ import {PartialProfile} from "../interfaces/Profile";
 import {v4 as uuidv4} from "uuid";
 
 
-export async function insertPartialProfile(partialProfile: PartialProfile) : Promise<string>{
+export async function insertPartialProfile(partialProfile: PartialProfile): Promise<string> {
     try {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
-        const query : string = `INSERT into profile(profileId, profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileImage)
+        const query: string = `INSERT into profile(profileId, profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileImage)
             VALUES((UUID_TO_BIN(${uuidv4()}), :profileId, :profileFirstName, :profileLastName, :profileLanguage, :profileEmailAddress, :profileImage)`;
         const [rows] = await mySqlConnection.execute(query, partialProfile);
         return "profile successfully inserted";
-    }
-    catch(error) {
+    } catch (error) {
         console.error(error.msg);
         return error.msg;
     }
