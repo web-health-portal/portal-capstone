52581dbdce8abd86a6cad1a4b51945ac16b4de51
Kyle Mayer <kmayer1@cnm.edu>
2021-02-25 14:12:20 -0700

    adding profile route to App.ts

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,13 +1,14 @@
-import express, { Application } from 'express'
+import express, {Application} from 'express'
 import morgan from 'morgan'
 // Routes
-import { indexRoute } from './apis/index.route'
+import {indexRoute} from './apis/index.route'
+import {ProfileRoute} from './apis/profile/profile.route'
 
 // The following class creates the app and instantiates the server
 export class App {
     app: Application;
 
-    constructor (
+    constructor(
         private port?: number | string
     ) {
         this.app = express()
@@ -17,24 +18,25 @@ export class App {
     }
 
     // private method that sets the port for the sever, to one from index.route.ts, and external .env file or defaults to 3000
-    public settings () {
+    public settings() {
         this.app.set('port', this.port || process.env.PORT || 4200)
     }
 
     // private method to setting up the middleware to handle json responses, one for dev and one for prod
-    private middlewares () {
+    private middlewares() {
         this.app.use(morgan('dev'))
         this.app.use(express.json())
     }
 
     // private method for setting up routes in their basic sense (ie. any route that performs an action on profiles starts with /profiles)
-    private routes () {
+    private routes() {
         // TODO add "/apis"
         this.app.use('/apis', indexRoute)
+        this.app.use('/apis/profile/', ProfileRoute)
     }
 
     // starts the server and tells the terminal to post a message that the server is running and on what port
-    public async listen (): Promise<void> {
+    public async listen(): Promise<void> {
         await this.app.listen(this.app.get('port'))
         console.log('Express application built successfully')
     }
