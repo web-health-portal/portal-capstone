97b18141b029872c06c8c531f72ad0b3e78aae0c
Kyle Mayer <kmayer1@cnm.edu>
2021-02-25 09:42:25 -0700

    added partial profile interface

--- backend/src/utils/interfaces/Profile.ts
+++ backend/src/utils/interfaces/Profile.ts
@@ -1,3 +1,13 @@
+export interface PartialProfile {
+    profileId: string,
+    profileFirstName: string,
+    profileLastName: string,
+    profileLanguage: string,
+    profileEmailAddress: string,
+    profileImage: string|null
+}
+
+
 export interface Profile {
     profileId: string,
     profileFirstName: string,
