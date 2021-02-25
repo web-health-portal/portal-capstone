f1cc51bd5ca65459dad37fbf59f0e86593e9fea6
Kyle Mayer <kmayer1@cnm.edu>
2021-02-25 10:43:31 -0700

    fixed return type and parameters for select profile functions

--- backend/src/utils/profile/getPartialProfileByProfileId.ts
+++ backend/src/utils/profile/getPartialProfileByProfileId.ts
@@ -1,15 +1,16 @@
 import {connect} from "../database.utils";
 import {PartialProfile} from "../interfaces/Profile";
 
-export async function getPartialProfileByProfileId(partialProfile: PartialProfile) : Promise<string>{
+export async function getPartialProfileByProfileId(profileId: string) {
     try {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
         const query : string = `SELECT profileId, profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileImage
-        FROM profile WHERE profileId=UUID_TO_BIN(${partialProfile.profileId})`;
-        const [rows] = await mySqlConnection.execute(query, partialProfile);
-        return JSON.stringify(partialProfile);
+        FROM profile WHERE profileId=UUID_TO_BIN(${profileId})`;
+        const [rows] = await mySqlConnection.execute(query, profileId);
+        // @ts-ignore is required so that rows can be interacted with like the array it is
+        return rows.length !== 0 ? {...rows[0]} : undefined;
     }
     catch(error) {
         console.error(error.msg);
