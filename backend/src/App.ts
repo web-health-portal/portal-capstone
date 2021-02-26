45dadc3e071cea8233e26aa471b66b2e97a52600
Kyle Mayer <kmayer1@cnm.edu>
2021-02-26 10:21:08 -0700

    added middlewares to App.ts

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -3,6 +3,8 @@ import morgan from 'morgan'
 // Routes
 import {indexRoute} from './apis/index.route'
 import {ProfileRoute} from './apis/profile/profile.route'
+import session, {MemoryStore} from "express-session";
+import passport from "passport";
 
 // The following class creates the app and instantiates the server
 export class App {
@@ -24,15 +26,29 @@ export class App {
 
     // private method to setting up the middleware to handle json responses, one for dev and one for prod
     private middlewares() {
-        this.app.use(morgan('dev'))
-        this.app.use(express.json())
+        const sessionConfig  =  {
+            store: new MemoryStore({
+                checkPeriod: 100800
+            }),
+            secret:"secret",
+            saveUninitialized: true,
+            resave: true,
+            maxAge: "3h"
+        };
+
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+        this.app.use(session(sessionConfig));
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
+        passport.use(passportStrategy);
     }
 
     // private method for setting up routes in their basic sense (ie. any route that performs an action on profiles starts with /profiles)
     private routes() {
-        // TODO add "/apis"
+        // TODO add our defined "/apis"/routes here
         this.app.use('/apis', indexRoute)
-        this.app.use('/apis/profile/', ProfileRoute)
+        this.app.use('/apis/profile', ProfileRoute)
     }
 
     // starts the server and tells the terminal to post a message that the server is running and on what port
