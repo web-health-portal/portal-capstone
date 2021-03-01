c9856405f96f4622b6a8a86d5a49413744f7b46b
Kyle Mayer <kmayer1@cnm.edu>
2021-03-01 16:36:41 -0700

    fixed MySQL enabled functions

--- backend/src/utils/profile/getProfileByProfileActivationToken.ts
+++ backend/src/utils/profile/getProfileByProfileActivationToken.ts
@@ -4,7 +4,7 @@ export async function getProfileByProfileActivationToken(profileActivationToken:
 	try {
 		const mysqlConnection = await connect();
 
-		const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileImage FROM profile WHERE profileActivationToken = :profileActivationToken', {profileActivationToken});
+		const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileHash, profileImage FROM profile WHERE profileActivationToken = :profileActivationToken', {profileActivationToken});
 
 		// @ts-ignore is required so that rows can be interacted with like the array it is
 		return rows.length !== 0 ? {...rows[0]} : undefined;
