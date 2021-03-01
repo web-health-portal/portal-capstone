2b13bd85501e51e9f9ee2597bac2554d590be0bc
Kyle Mayer <kmayer1@cnm.edu>
2021-03-01 16:12:50 -0700

    changed updateProfile

--- backend/src/utils/profile/updateProfile.ts
+++ backend/src/utils/profile/updateProfile.ts
@@ -7,7 +7,7 @@ export async function updateProfile(profile: Profile): Promise<string> {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
-        const query : string = 'UPDATE profile SET profileFirstName=:profileFirstName, profileLastName=:profileLastName, profileLanguage=:profileLanguage, profileEmailAddress=:profileEmailAddress, profileActivationToken=:profileActivationToken, profileHash=:profileHash, profileImage=:profileImage WHERE profileId=UUID_TO_BIN(:profileId)';
+        const query : string = 'UPDATE profile SET profileActivationToken = :profileActivationToken, profileFirstName = :profileFirstName, profileLastName = :profileLastName, profileEmailAddress = :profileEmailAddress, profileHash = :profileHash, profileImage = :profileImage, profileLanguage = :profileLanguage WHERE UUID_TO_BIN(:profileId)';
         const [rows] = await mySqlConnection.execute(query, profile);
         return "profile successfully updated";
     }
