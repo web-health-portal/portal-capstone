3bd3363eb1a05125ede0ddb7aa98fa90a6d849d3
Kyle Mayer <kmayer1@cnm.edu>
2021-03-02 07:49:05 -0700

    adding fix for insertArticle function

--- backend/src/utils/article/insertArticle.ts
+++ backend/src/utils/article/insertArticle.ts
@@ -1,21 +1,16 @@
 import {connect} from "../database.utils";
 import {Article} from "../interfaces/Article";
-import { v4 as uuidv4 } from 'uuid';
 
 export async function insertArticle(article: Article) : Promise<string>{
     try {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
-        const query : string = `INSERT into article (articleId, articleEnglishTitle, articleEnglishDate, articleEnglishImageUrl, articleEnglishImageAlt, articleSpanishTitle, articleSpanishDate, articleSpanishImageUrl, articleSpanishUrl)
-            VALUES((UUID_TO_BIN(${uuidv4()}), :articleEnglishId, :articleEnglishTitle, :articleEnglishDate, :articleEnglishImageUrl, :articleEnglishImageAlt, :articleSpanishTitle, :articleSpanishDate, :articleSpanishUrl)`;
-        // const [rows] = await mySqlConnection.execute(query, article);
-        console.log();
-        console.log("article successfully inserted");
+        const query : string = 'INSERT into article (articleId, articleEnglishTitle, articleEnglishDate, articleEnglishImageUrl, articleEnglishImageAlt, articleSpanishTitle, articleSpanishDate, articleSpanishImageUrl, articleSpanishUrl) VALUES(UUID_TO_BIN(UUID()), :articleEnglishId, :articleEnglishTitle, :articleEnglishDate, :articleEnglishImageUrl, :articleEnglishImageAlt, :articleSpanishTitle, :articleSpanishDate, :articleSpanishUrl)';
+        const [rows] = await mySqlConnection.execute(query, article);
         return "article successfully inserted";
     }
     catch(error) {
-        console.error(error.msg);
-        return error.msg;
+        throw new Error(error);
     }
 }
