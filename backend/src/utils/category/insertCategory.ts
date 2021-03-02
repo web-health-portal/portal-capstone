8b2d4764694f0240a6ba299bb890277984895618
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-03-02 16:35:37 -0700

    modified insertCategory.ts

--- backend/src/utils/category/insertCategory.ts
+++ backend/src/utils/category/insertCategory.ts
@@ -4,7 +4,7 @@ import {connect} from "../database.utils";
 export async function insertCategory(category: Category) {
     try {
         const mySqlConnection = await connect()
-        const mySqlQuery = "INSERT INTO category(categoryId, categoryEnglishName,categorySpanishName) VALUES(UUID_TO_BIN(UUID()), UUID_TO_BIN(:categoryId), :categoryEnglishName, :categorySpanishName, NOW())";
+        const mySqlQuery = "INSERT INTO category(categoryId, categoryEnglishName,categorySpanishName) VALUES(UUID_TO_BIN(UUID()), :categoryEnglishName, :categorySpanishName)";
 
         const [rows] = await mySqlConnection.execute(mySqlQuery, category)
         return "Category successfully inserted"
