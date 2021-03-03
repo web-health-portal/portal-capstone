e1faef18dc7a70abb8f91068a2794b9f21df1d99
Kyle Mayer <kmayer1@cnm.edu>
2021-03-03 13:35:28 -0700

    added MySQL function for insertArticleCategory

---
+++ backend/src/utils/article/insertArticleCategory.ts
@@ -0,0 +1,16 @@
+import {connect} from "../database.utils";
+import {ArticleCategory} from "../interfaces/ArticleCategory";
+
+export async function insertArticle(articleCategory: ArticleCategory) : Promise<string>{
+    try {
+        //establish connection with the database
+        const mySqlConnection = await connect();
+        //query is a string for holding a sql statement (prepared statement)
+        const query : string = 'INSERT into  articleCategory(articleCategoryCategoryId, articleCategoryArticleId) VALUES(:articleCategoryCategoryId, :articleCategoryArticleId)';
+        const [rows] = await mySqlConnection.execute(query, articleCategory);
+        return "articleCategory successfully inserted";
+    }
+    catch(error) {
+        throw new Error(error);
+    }
+}
