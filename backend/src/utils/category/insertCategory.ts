42af0802c4056f40be054de39ffdfe49d3e09ebc
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-03-02 17:28:13 -0700

    made changes to category.route and added categoryMyHealthFinder to insertCategory

--- backend/src/utils/category/insertCategory.ts
+++ backend/src/utils/category/insertCategory.ts
@@ -4,7 +4,7 @@ import {connect} from "../database.utils";
 export async function insertCategory(category: Category) {
     try {
         const mySqlConnection = await connect()
-        const mySqlQuery = "INSERT INTO category(categoryId, categoryEnglishName,categorySpanishName) VALUES(UUID_TO_BIN(UUID()), :categoryEnglishName, :categorySpanishName)";
+        const mySqlQuery = "INSERT INTO category(categoryId, categoryEnglishName,categorySpanishName,categoryMyHealthFinderId) VALUES(UUID_TO_BIN(UUID()), :categoryEnglishName, :categorySpanishName, :categoryMyHealthFinderId)";
 
         const [rows] = await mySqlConnection.execute(mySqlQuery, category)
         return "Category successfully inserted"
