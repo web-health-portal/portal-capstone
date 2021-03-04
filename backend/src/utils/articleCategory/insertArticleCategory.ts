c5567527f757e4cd14d7444f10d8fc4ef91a4af5
Kyle Mayer <kmayer1@cnm.edu>
2021-03-04 10:24:50 -0700

    added code to close connection to database for ArticleCategory

--- backend/src/utils/articleCategory/insertArticleCategory.ts
+++ backend/src/utils/articleCategory/insertArticleCategory.ts
@@ -1,16 +1,16 @@
 import {connect} from "../database.utils";
 import {ArticleCategory} from "../interfaces/ArticleCategory";
 
-export async function insertArticleCategory(articleCategory: ArticleCategory) : Promise<string>{
+export async function insertArticleCategory(articleCategory: ArticleCategory): Promise<string> {
     try {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
-        const query : string = 'INSERT into  articleCategory(articleCategoryCategoryId, articleCategoryArticleId) VALUES(UUID_TO_BIN(:articleCategoryCategoryId), UUID_TO_BIN(:articleCategoryArticleId))';
+        const query: string = 'INSERT into  articleCategory(articleCategoryCategoryId, articleCategoryArticleId) VALUES(UUID_TO_BIN(:articleCategoryCategoryId), UUID_TO_BIN(:articleCategoryArticleId))';
         const [rows] = await mySqlConnection.execute(query, articleCategory);
+        await mySqlConnection.end();
         return "articleCategory successfully inserted";
-    }
-    catch(error) {
+    } catch (error) {
         throw new Error(error);
     }
 }
