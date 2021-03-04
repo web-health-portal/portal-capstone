195dff0a12b765dafb945d13259029d29db44974
Kyle Mayer <kmayer1@cnm.edu>
2021-03-04 10:23:13 -0700

    added code to close connection to database for Article and Category

--- backend/src/utils/article/insertArticle.ts
+++ backend/src/utils/article/insertArticle.ts
@@ -6,8 +6,9 @@ export async function insertArticle(article: Article) : Promise<string>{
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
-        const query : string = 'INSERT into article (articleId, articleEnglishTitle, articleEnglishDate, articleEnglishImageUrl, articleEnglishImageAlt, articleSpanishTitle, articleSpanishDate, articleSpanishImageUrl, articleSpanishUrl) VALUES(UUID_TO_BIN(:articleId), :articleEnglishTitle, :articleEnglishDate, :articleEnglishImageUrl, :articleEnglishImageAlt, :articleSpanishTitle, :articleSpanishDate, :articleSpanishImageUrl, :articleSpanishUrl)';
+        const query : string = 'INSERT into article (articleId, articleEnglishTitle, articleEnglishDate, articleEnglishImageUrl, articleEnglishImageAlt, articleMyHealthFinderId, articleSpanishTitle, articleSpanishDate, articleSpanishImageUrl, articleSpanishUrl) VALUES(UUID_TO_BIN(:articleId), :articleEnglishTitle, :articleEnglishDate, :articleEnglishImageUrl, :articleEnglishImageAlt, :articleMyHealthFinderId, :articleSpanishTitle, :articleSpanishDate, :articleSpanishImageUrl, :articleSpanishUrl)';
         const [rows] = await mySqlConnection.execute(query, article);
+        await mySqlConnection.end();
         return "article successfully inserted";
     }
     catch(error) {
--- backend/src/utils/category/insertCategory.ts
+++ backend/src/utils/category/insertCategory.ts
@@ -9,6 +9,7 @@ export async function insertCategory(category: Category) {
         const mySqlQuery = "INSERT INTO category(categoryId, categoryEnglishName,categorySpanishName) VALUES(UUID_TO_BIN(:categoryId), :categoryEnglishName, :categorySpanishName)";
 
         const [rows] = await mySqlConnection.execute(mySqlQuery, category)
+        await mySqlConnection.end();
         return "Category successfully inserted"
     } catch (error) {
         console.log(error)
