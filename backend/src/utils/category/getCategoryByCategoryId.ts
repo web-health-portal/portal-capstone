a7d6d59776963b6588d31176a2014f98346bdcf6
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-03-04 00:52:19 -0700

    modified category rest api

--- backend/src/utils/category/getCategoryByCategoryId.ts
+++ backend/src/utils/category/getCategoryByCategoryId.ts
@@ -5,7 +5,7 @@ export async function getCategoryByCategoryId(categoryId: string) {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
-        const query: string = 'SELECT categoryId, categoryEnglishName, categorySpanishName FROM category WHERE categoryId=UUID_TO_BIN(:categoryId)';
+        const query: string = 'SELECT BIN_TO_UUID(categoryId) AS  categoryId, categoryEnglishName, categorySpanishName FROM category WHERE categoryId=UUID_TO_BIN(:categoryId)';
         const [rows] = await mySqlConnection.execute(query,{categoryId});
         // @ts-ignore is required so that rows can be interacted with like the array it is
         return rows.length !== 0 ? {...rows[0]} : undefined;
