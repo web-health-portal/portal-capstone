9945097cf96273e2f82fb96a07e765a0769ae53d
Kyle Mayer <kmayer1@cnm.edu>
2021-03-04 14:44:04 -0700

    modified MySql functions for article

---
+++ backend/src/utils/article/getArticleMyHealthFinderIdByMyHealthFinderId.ts
@@ -0,0 +1,13 @@
+import {connect} from "../database.utils";
+
+export async function getArticleMyHealthFinderIdByMyHealthFinderId(articleMyHealthFinderId: string) {
+	try {
+		const mySqlConnection = await connect();
+        const [rows] = await mySqlConnection.execute('SELECT  articleMyHealthFinderId FROM article WHERE articleMyHealthFinderId = :articleMyHealthFinderId', {articleMyHealthFinderId});
+		await mySqlConnection.end();
+		// @ts-ignore is required so that rows can be interacted with like the array it is
+		return rows.length !== 0 ? {...rows[0]} : {articleMyHealthFinderId: ""};
+	} catch (error) {
+	    throw new Error(error);
+	}
+}
