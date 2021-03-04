8660442c6a6cf31670d1aafa415c42a488ec07a6
Kyle Mayer <kmayer1@cnm.edu>
2021-03-04 16:13:19 -0700

    fixed function for getProfileByProfileId

--- backend/src/utils/profile/getProfileByProfileId.ts
+++ backend/src/utils/profile/getProfileByProfileId.ts
@@ -1,13 +1,11 @@
 import {connect} from "../database.utils";
-import {Profile} from "../interfaces/Profile";
 
 export async function getProfileByProfileId(profileId: string) {
     try {
         //establish connection with the database
         const mySqlConnection = await connect();
         //query is a string for holding a sql statement (prepared statement)
-        const query: string = 'SELECT profileId, profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileActivationToken, profileHash, profileImage FROM profile WHERE profileId=UUID_TO_BIN(:profileId)';
-        const [rows] = await mySqlConnection.execute(query, profileId);
+        const [rows] = await mySqlConnection.execute('SELECT BIN_TO_UUID(profileId), profileFirstName, profileLastName, profileLanguage, profileEmailAddress, profileActivationToken, profileHash, profileImage FROM profile WHERE profileId=UUID_TO_BIN(:profileId)', {profileId});
         await mySqlConnection.end();
         // @ts-ignore is required so that rows can be interacted with like the array it is
         return rows.length !== 0 ? {...rows[0]} : undefined;
