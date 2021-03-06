a0bfc550f4a2b3882a20af0b5642d006c3be7199
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-03-06 14:52:17 -0700

    modified category ts

--- backend/src/utils/interfaces/Category.ts
+++ backend/src/utils/interfaces/Category.ts
@@ -1,9 +1,5 @@
 export interface Category {
-<<<<<<< HEAD
-    categoryId: string | null,
-=======
     categoryId: string,
->>>>>>> bcea08b71971d38a559927eed2460b900e381289
     categoryEnglishName: string,
     categorySpanishName: string
 }
