69be751c81e72a8dc959f530be5a9912c7d0270b
Kyle Mayer <kmayer1@cnm.edu>
2021-03-07 12:27:14 -0700

    fixed getProfileByProfileEmail function. Log-in now works! Yay

--- backend/src/utils/profile/getProfileByProfileEmail.ts
+++ backend/src/utils/profile/getProfileByProfileEmail.ts
@@ -1,11 +1,11 @@
 import {connect} from "../database.utils";
 import {Profile} from "../interfaces/Profile";
 
-export async function getProfileByProfileEmail(profileEmail: string) {
+export async function getProfileByProfileEmail(profileEmailAddress: string) {
 	try {
 		const mysqlConnection = await connect();
 
-		const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileFirstName, profileEmailAddress, profileLastName, profileImage FROM profile WHERE profileEmailAddress = :profileEmail', {profileEmail});
+		const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileFirstName, profileEmailAddress, profileLastName, profileImage, profileHash FROM profile WHERE profileEmailAddress = :profileEmailAddress', {profileEmailAddress});
 		await mysqlConnection.end();
 		// @ts-ignore is required so that rows can be interacted with like the array it is
 		return rows.length !== 0 ? {...rows[0]} : undefined;
