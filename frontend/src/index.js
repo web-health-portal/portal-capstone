0fb7086fe80c2f3df7fae126a4487c861e0c7d70
Kyle Mayer <kmayer1@cnm.edu>
2021-03-08 11:42:47 -0700

    rollback changes from merge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
 import ReactDOM from 'react-dom'
 import {App} from "./ui/App"
-import store from "./store/store"
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 
-ReactDOM.render(App(store), document.querySelector('#root'));
+ReactDOM.render(App(), document.querySelector('#root'));
--- frontend/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import profile from "./profile"
-import auth from "./auth";
-import { configureStore,combineReducers} from '@reduxjs/toolkit'
-
-const reducer = combineReducers({auth, profile})
-export default configureStore({reducer});
