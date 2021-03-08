1ea6734861636d88ac647abd5eab200a6309387f
Kyle Mayer <kmayer1@cnm.edu>
2021-03-08 13:36:49 -0700

    added translation for spanish search results

--- frontend/src/ui/search-results/SearchResultsSpanish.js
+++ frontend/src/ui/search-results/SearchResultsSpanish.js
@@ -24,19 +24,19 @@ const placeHolderArticle = {
     ],
 }
 
-export const SearchResults = ({keyword}) => {
+export const SearchResultsSpanish = ({keyword}) => {
     //set prop to dummy value for now
     keyword = "diabetes";
     return (
         <>
             <Container className={"pt-4"}>
                 <Col md={8} className={"pt-3"}>
-                    <h3>Search Results for {keyword}</h3>
+                    <h3>Resultados de búsqueda para {keyword}</h3>
                     <Row>
                         <Form inline className={"mx-3"}>
                             <FormGroup className={"container d-flex justify-content-between"}>
                                 <FormControl type="text" placeholder="Another Search" className="mr-md-2"/>
-                                <Button variant="primary" className={"py-sm-2 ml-2"}>Search</Button>
+                                <Button variant="primary" className={"py-sm-2 ml-2"}>Buscar</Button>
                             </FormGroup>
                         </Form>
                     </Row>
