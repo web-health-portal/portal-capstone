f513fbf359fcbec4d9d219701fa556430bb86437
Elizabeth Hines <ehines5@cnm.edu>
2021-03-09 10:21:20 -0700

    finished selectArticle function

--- backend/src/utils/saved-article/selectSavedArticleByArticleIdAndProfileId.ts
+++ backend/src/utils/saved-article/selectSavedArticleByArticleIdAndProfileId.ts
@@ -4,9 +4,11 @@ import {SavedArticle} from "../interfaces/SavedArticle";
 export async function selectSavedArticleByArticleIdAndProfileId(savedArticle: SavedArticle) {
     try {
         const mySqlConnection = await connect()
-        const mySqlQuery = "SELECT FROM `savedArticle` WHERE savedArticleArticleId = UUID_TO_BIN(:savedArticleArticleId) AND savedArticleProfileId = UUID_TO_BIN(:savedArticleProfileId)";
+        const mySqlQuery = "SELECT savedArticleArticleId, savedArticleProfileId FROM `savedArticle` WHERE savedArticleArticleId = UUID_TO_BIN(:savedArticleArticleId) AND savedArticleProfileId = UUID_TO_BIN(:savedArticleProfileId)";
 
-        await mySqlConnection.execute(mySqlQuery, savedArticle)
+        const [rows] = await mySqlConnection.execute(mySqlQuery, savedArticle)
+        await mySqlConnection.end()
+        return rows
     }
 
     catch (error) {
