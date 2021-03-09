b7329012fecde514ca651cbdb4cd5ca68cbd091f
Elizabeth Hines <ehines5@cnm.edu>
2021-03-08 17:23:09 -0700

    deleted uuid for profileId

--- backend/src/utils/article/selectArticlesByProfileId.ts
+++ backend/src/utils/article/selectArticlesByProfileId.ts
@@ -3,7 +3,7 @@ import {connect} from "../database.utils";
 export async function selectArticlesByProfileId(profileId: string) {
     try {
         const mySqlConnection = await connect();
-        const query : string = "SELECT BIN_TO_UUID(savedArticle.savedArticleProfileId) as profileId, BIN_TO_UUID(article.articleId) as articleId, articleEnglishTitle, articleEnglishDate, articleEnglishImageUrl, articleEnglishImageAlt, articleMyHealthFinderId, articleSpanishTitle, articleSpanishDate, articleSpanishImageUrl, articleSpanishUrl FROM article INNER JOIN savedArticle ON article.articleId = savedArticle.savedArticleArticleId WHERE savedArticleProfileId = :profileId";
+        const query : string = "SELECT BIN_TO_UUID(article.articleId) as articleId, articleEnglishTitle, articleEnglishDate, articleEnglishImageUrl, articleEnglishImageAlt, articleMyHealthFinderId, articleSpanishTitle, articleSpanishDate, articleSpanishImageUrl, articleSpanishUrl FROM savedArticle INNER JOIN article ON article.articleId = savedArticle.savedArticleArticleId WHERE savedArticleProfileId = :profileId";
 
         const [rows] = await mySqlConnection.execute(query, {profileId: profileId});
         await mySqlConnection.end();
