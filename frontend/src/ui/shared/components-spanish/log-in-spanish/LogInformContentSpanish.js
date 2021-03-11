f769b9f337162d3922594470b4b60f3f54cc62e0
RamonPantoja12 <75707891+RamonPantoja12@users.noreply.github.com>
2021-03-10 23:11:45 -0700

    modified logInSpanish formik

---
+++ frontend/src/ui/shared/components-spanish/log-in-spanish/LogInformContentSpanish.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import {Modal, Form, Button} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {LogInFormSpanish} from "./LogInFormSpanish";
+
+export const LogInModalSpanish = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Link className={"nav-link px-3"} onClick={handleShow}>
+                Log In
+            </Link>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Welcome back!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {/*form for email and password*/}
+                    <LogInFormSpanish/>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email"/>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password"/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleClose}>Log In</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
