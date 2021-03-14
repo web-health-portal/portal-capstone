5352e80257b95947272c12d15f26c2e75edc1245
Kyle Mayer <kmayer1@cnm.edu>
2021-03-14 09:24:42 -0600

    switching between account spanisha and english now works

--- frontend/src/ui/account/Account.js
+++ frontend/src/ui/account/Account.js
@@ -7,6 +7,8 @@ import {Article} from "../shared/components/Article";
 import {ArticleSpanish} from "../shared/components-spanish/ArticleSpanish"
 import savedArticle, {fetchAllSavedArticles} from "../../store/savedArticle/savedArticle";
 import {useDispatch, useSelector} from "react-redux";
+import {AccountSpanish} from "./AccountSpanish";
+import {AccountEnglish} from "./AccountEnglish";
 
 library.add(faCamera, faUser);
 
@@ -33,68 +35,13 @@ export const Account = () => {
 
     return (
         <>
-            <h1 className={"mx-3"}>Account</h1>
-            <Container as={"section"} fluid className={"p-3"}>
-                <Row>
-                    <Col md={4} className={"border-right border-dark md-auto"}>
-                        <Row className={"justify-content-center align-items-center"}>
-                            <FontAwesomeIcon icon={faUser} size="10x" className={"p-4 border rounded-circle"}/>
-                            <FontAwesomeIcon icon={faCamera} size="lg" className={"mx-2"}/>
-                        </Row>
-                        <Row className={"mt-1 justify-content-center"}>
-                            <Form className={"px-3"}>
-                                <FormGroup>
-                                    <Form.Label>Name:</Form.Label>
-                                    <Form.Control type={"text"} placeholder={"Name"}/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Form.Label>Email:</Form.Label>
-                                    <Form.Control type={"email"} placeholder={"name@your-email.com"}/>
-                                </FormGroup>
-                                <Form.Group controlId="language-selector">
-                                    <Form.Label>Language:</Form.Label>
-                                    <Form.Control as="select">
-                                        <option>English</option>
-                                        <option>Español</option>
-                                    </Form.Control>
-                                </Form.Group>
-                                <Button variant={"primary"}>Save</Button>
-                            </Form>
-                        </Row>
-                        <Row className={"mt-1"}>
-                        </Row>
-                    </Col>
-                    <Col md={8} className={"pt-3"}>
-                        <h3>Search Saved Articles</h3>
-                        <Row>
-                            <Form inline className={"mx-3"}>
-                                <FormGroup className={"container d-flex justify-content-between"}>
-                                    <FormControl type="text" placeholder="Search Your Articles" className="mr-md-2"/>
-                                    <Button variant="primary" className={"py-sm-2 ml-2"}>Search</Button>
-                                </FormGroup>
-                            </Form>
-                        </Row>
-                        <Row>
-                            <Container>
-                                <Row>
-                                    <Col>
-                                        {
-                                            toggle === true && spanishSavedArticle.map(article => <ArticleSpanish
-                                                key={article.articleId}
-                                                article={article}/>)
-                                            ||
-                                                toggle === false && englishSavedArticle.map(article => <Article
-                                                key={article.articleId}
-                                                article={article}/>)
-                                        }
-                                    </Col>
-                                </Row>
-                            </Container>
-                        </Row>
-                    </Col>
-                </Row>
 
-            </Container>
+            {
+                toggle === true && <AccountSpanish spanishSavedArticle={spanishSavedArticle}/>
+                ||
+                toggle === false && <AccountEnglish englishSavedArticle={englishSavedArticle}/>
+            }
+
         </>
     )
 }
--- frontend/src/ui/account/AccountEnglish.js
+++ frontend/src/ui/account/AccountEnglish.js
@@ -3,21 +3,21 @@ import {Form, Button, Col, Container, FormControl, FormGroup, Row} from "react-b
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faUser, faCamera} from "@fortawesome/free-solid-svg-icons";
-import {ArticleSpanish} from "../shared/components-spanish/ArticleSpanish";
+import {Article} from "../shared/components/Article";
+import {ArticleSpanish} from "../shared/components-spanish/ArticleSpanish"
+import savedArticle, {fetchAllSavedArticles} from "../../store/savedArticle/savedArticle";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchAllSavedArticles} from "../../store/savedArticle/savedArticle";
 
 library.add(faCamera, faUser);
 
 
-export const AccountSpanish = (props) => {
+export const AccountEnglish = (props) => {
 
-    // subscribe using useSelector to the slice of store you care about
-    const {spanishSavedArticle} = props;
+    const {englishSavedArticle} = props;
 
     return (
         <>
-            <h1 className={"mx-3"}>Perfil</h1>
+            <h1 className={"mx-3"}>Account</h1>
             <Container as={"section"} fluid className={"p-3"}>
                 <Row>
                     <Col md={4} className={"border-right border-dark md-auto"}>
@@ -28,33 +28,33 @@ export const AccountSpanish = (props) => {
                         <Row className={"mt-1 justify-content-center"}>
                             <Form className={"px-3"}>
                                 <FormGroup>
-                                    <Form.Label>Nombre:</Form.Label>
-                                    <Form.Control type={"text"} placeholder={"Nombre"}/>
+                                    <Form.Label>Name:</Form.Label>
+                                    <Form.Control type={"text"} placeholder={"Name"}/>
                                 </FormGroup>
                                 <FormGroup>
-                                    <Form.Label>Correo electrónico:</Form.Label>
-                                    <Form.Control type={"email"} placeholder={"su-correo@dominio.com"}/>
+                                    <Form.Label>Email:</Form.Label>
+                                    <Form.Control type={"email"} placeholder={"name@your-email.com"}/>
                                 </FormGroup>
                                 <Form.Group controlId="language-selector">
-                                    <Form.Label>Idioma:</Form.Label>
+                                    <Form.Label>Language:</Form.Label>
                                     <Form.Control as="select">
                                         <option>English</option>
                                         <option>Español</option>
                                     </Form.Control>
                                 </Form.Group>
-                                <Button variant={"primary"}>Guardar</Button>
+                                <Button variant={"primary"}>Save</Button>
                             </Form>
                         </Row>
                         <Row className={"mt-1"}>
                         </Row>
                     </Col>
                     <Col md={8} className={"pt-3"}>
-                        <h3>Buscar Artículos Guardados</h3>
+                        <h3>Search Saved Articles</h3>
                         <Row>
                             <Form inline className={"mx-3"}>
                                 <FormGroup className={"container d-flex justify-content-between"}>
-                                    <FormControl type="text" placeholder="Busque sus articulos" className="mr-md-2"/>
-                                    <Button variant="primary" className={"py-sm-2 ml-2"}>Buscar</Button>
+                                    <FormControl type="text" placeholder="Search Your Articles" className="mr-md-2"/>
+                                    <Button variant="primary" className={"py-sm-2 ml-2"}>Search</Button>
                                 </FormGroup>
                             </Form>
                         </Row>
@@ -63,8 +63,8 @@ export const AccountSpanish = (props) => {
                                 <Row>
                                     <Col>
                                         {
-                                            spanishSavedArticle.map(article => <ArticleSpanish key={article.articleId}
-                                                                                               article={article}/>)
+                                            englishSavedArticle.map(article => <Article key={article.articleId}
+                                                                                        article={article}/>)
                                         }
                                     </Col>
                                 </Row>
