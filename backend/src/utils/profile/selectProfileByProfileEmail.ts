a2da5724292d5634e0d908132ab8c1296562eb7b
Kyle Mayer <kmayer1@cnm.edu>
2021-03-17 14:49:36 -0600

    added some more to the jwt token

--- backend/src/utils/profile/selectProfileByProfileEmail.ts
+++ backend/src/utils/profile/selectProfileByProfileEmail.ts
@@ -5,7 +5,7 @@ export async function selectProfileByProfileEmail(profileEmailAddress: string) {
 	try {
 		const mysqlConnection = await connect();
 
-		const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileFirstName, profileEmailAddress, profileLastName, profileImage, profileHash FROM profile WHERE profileEmailAddress = :profileEmailAddress', {profileEmailAddress});
+		const [rows] = await mysqlConnection.execute('SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileFirstName, profileEmailAddress, profileLastName, profileImage, profileLanguage, profileHash FROM profile WHERE profileEmailAddress = :profileEmailAddress', {profileEmailAddress});
 		await mysqlConnection.end();
 		// @ts-ignore is required so that rows can be interacted with like the array it is
 		return rows.length !== 0 ? {...rows[0]} : undefined;
