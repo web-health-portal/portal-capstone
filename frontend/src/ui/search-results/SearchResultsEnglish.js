de380f34551761f3e9181b2b06eeb4b28d9529cc
Elizabeth Hines <ehines5@cnm.edu>
2021-03-17 16:39:08 -0600

    further fixed search results conflict

--- frontend/src/ui/search-results/SearchResultsEnglish.js
+++ frontend/src/ui/search-results/SearchResultsEnglish.js
@@ -11,7 +11,7 @@ export const SearchResultsEnglish = (props) => {
         <>
             <Container className={"pt-4 border rounded drop-shadow"}>
                 <Col md={8} className={"pt-3"}>
-                    <h3>Search Results for {keyword}</h3>
+                    <h3>Search Results</h3>
                     <Row>
                         <SearchBar/>
                     </Row>
@@ -23,21 +23,6 @@ export const SearchResultsEnglish = (props) => {
                         englishArticle.map(article => <ArticleEnglish key={article.articleId} article={article}/>)
                     }
                 </Col>
-            </Container>
-        <Container className={"pt-4"}>
-            <Col md={8} className={"pt-3"}>
-                <h3>Search Results</h3>
-                <Row>
-                    <SearchBar/>
-                </Row>
-            </Col>
-        </Container>
-        <Container>
-            <Col>
-                {
-                    englishArticle.map(article => <ArticleEnglish key={article.articleId} article={article}/>)
-                }
-            </Col>
             </Container>
 
         </>
